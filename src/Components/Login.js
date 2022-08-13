dba4ed0f769c4f5f49aa42e9fd208e6070362a9b
PavaneeshwarReddy <pavaneeshwar7077@gmail.com>
2022-08-13 06:46:06 -0400

    after loginPage

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import "../CSS/Login.css"
+import LoginLogo from '../Images/login1.png'
+import googlelogo from '../Images/gogolelogo.png'
+import {useNavigate} from 'react-router-dom'
+import {GoogleButton} from 'react-google-button'
 function Login() {
+  const navigate = useNavigate();
+  const gotoblogs =()=>{
+    navigate("/blogs");
+  }
   return (
-    <div>Login</div>
+    <div className='loginpage'>
+    
+                <div className='loginbox'>
+                      <img className='loginimage' src={LoginLogo}></img>
+                      <div className='googlebuttonflex'>
+                            <img className='googlelogo' src={googlelogo}></img>
+                            <button className='googlebutton' onClick={gotoblogs}>Signin with google</button>
+
+                      </div>
+                </div>
+    </div>
   )
 }
 
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -8,7 +8,7 @@ function NavBar() {
         <div className='navbar'>
             <div className='navbar_items'>
                 <div className='navbar_items_start'>
-                <h2 className='CompanyName'>BlogSpot</h2>
+                <Link to='/'><h2 className='CompanyName'>BlogSpot</h2></Link>
                 </div>
                 <div className='navbar_items_end'>
                 <Link to="/">Home</Link>
